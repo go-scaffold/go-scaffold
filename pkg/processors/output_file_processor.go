d4ce4e33350edc74a161173f1a795695491ad81f
pasdam <pasdam@users.noreply.github.com>
2021-04-20 21:29:56 +0800

    Group all processors in the same package

--- pkg/processors/output_file_processor.go
+++ pkg/processors/output_file_processor.go
@@ -1,23 +1,21 @@
-package scaffold
+package processors
 
 import (
 	"io"
 	"path/filepath"
-
-	"github.com/pasdam/go-scaffold/pkg/processors"
 )
 
 type outputFileProcessor struct {
 	outDir             string
-	tempplateProcessor processors.Processor
+	tempplateProcessor Processor
 }
 
 // NewOutputFileProcessor creates a new instance of a FileProcessor that process
 // templates and creates the output files.
 // THe variables to use for the template are in config.
-func NewOutputFileProcessor(config interface{}, outDir string) processors.Processor {
-	writeProcessor := processors.NewWriteProcessor()
-	templateProcessor := processors.NewTemplateProcessor(config, writeProcessor)
+func NewOutputFileProcessor(config interface{}, outDir string) Processor {
+	writeProcessor := NewWriteProcessor()
+	templateProcessor := NewTemplateProcessor(config, writeProcessor)
 
 	return &outputFileProcessor{
 		outDir:             outDir,
