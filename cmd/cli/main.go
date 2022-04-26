4120eaa9465a762adcfdb4ffb7ef2a353da7a488
pasdam <pasdam@users.noreply.github.com>
2022-04-26 23:14:53 +0200

    Move generate method into main file

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pasdam/go-scaffold/pkg/app"
+	"github.com/pasdam/go-scaffold/pkg/config"
 	"github.com/spf13/cobra"
 )
 
@@ -26,4 +28,26 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func generate(cmd *cobra.Command, args []string) {
+	if len(args) != 2 {
+		cmd.PrintErr("Invalid number of command arguments")
+		cmd.Help()
+		return
+	}
+
+	values, err := cmd.Flags().GetStringArray("values")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	options := &config.Options{
+		OutputPath:       args[1],
+		TemplateRootPath: args[0],
+		Values:           values,
+	}
+
+	app.Run(options)
 }
