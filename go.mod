8f6ca42615f7f78849406058779183f16c7d77c6
pasdam <pasdam@users.noreply.github.com>
2022-04-26 23:14:35 +0200

    Add additional template functions

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Masterminds/sprig/v3 v3.2.2
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/iancoleman/strcase v0.1.3
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
--- pkg/app/new_output_pipeline.go
+++ pkg/app/new_output_pipeline.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"strings"
+
 	"github.com/Masterminds/sprig/v3"
+	"github.com/iancoleman/strcase"
 	"github.com/pasdam/go-scaffold/pkg/core"
 	"github.com/pasdam/go-scaffold/pkg/filters"
 	"github.com/pasdam/go-scaffold/pkg/helpers/collections"
@@ -11,6 +14,10 @@ import (
 func newOutputPipeline(config interface{}, outDir string) (core.Processor, error) {
 	filter := filters.NewNoOpFilter()
 	funcMap := sprig.TxtFuncMap()
+	funcMap["camelcase"] = strcase.ToCamel
+	funcMap["replace"] = func(target string, replacement string, source string) string {
+		return strings.ReplaceAll(source, target, replacement)
+	}
 	funcMap["sequence"] = collections.Sequence
 	outProcessor := processors.NewOutputFileProcessor(config, outDir, funcMap)
 	return processors.NewFilterProcessor(filter, outProcessor), nil
